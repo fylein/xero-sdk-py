0a89ae77b0a88f9c1c6c145cd730a6f7ea4ecccc
sanjeev29 <sanjeev.raichur@codemonk.in>
2020-01-06 18:54:03 +0530

    Feat: Get all invoices method
    - Add a method in Invoices class to get all xero invoices

--- xerosdk/apis/invoices.py
+++ xerosdk/apis/invoices.py
@@ -3,8 +3,12 @@ from .api_base import ApiBase
 
 class Invoices(ApiBase):
 
+    GET_INVOICES = '/api.xro/2.0/invoices'
     GET_INVOICE_BY_ID = '/api.xro/2.0/invoices/{0}'
     POST_INVOICE = '/api.xro/2.0/invoices'
+
+    def get_all(self):
+        return self._get_request(Invoices.GET_INVOICES)
 
     def get_by_id(self, invoice_id):
         return self._get_request(Invoices.GET_INVOICE_BY_ID.format(invoice_id))
