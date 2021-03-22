003ae901515b132f0103f951b7e658c18efeed20
ashwinthanaraj <ashwin.t@fyle.in>
2021-03-22 19:05:22 +0530

    Support for post of accounts

--- xerosdk/apis/accounts.py
+++ xerosdk/apis/accounts.py
@@ -11,6 +11,7 @@ class Accounts(ApiBase):
     """
 
     GET_ACCOUNTS = "/api.xro/2.0/accounts"
+    POST_ACCOUNTS = "/api.xro/2.0/Accounts"
 
     def get_all(self):
         """
@@ -21,3 +22,17 @@ class Accounts(ApiBase):
         """
 
         return self._get_request(Accounts.GET_ACCOUNTS)
+
+
+    def post(self, data):
+        """
+        create new account
+
+        Parameters:
+        data (dict): Data to create account
+
+        Returns:
+             Response from API
+        """
+
+        return self._update_request(data, Accounts.POST_ACCOUNTS)
